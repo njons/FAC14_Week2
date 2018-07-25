a9a95734c9b41643445ebd4c11fe7f7f1f403794
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-07-25 21:58:32 +0100

    setup tests file

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
-
+// tape is required to be used as the tets framework
 var test = require('tape');
+// this is where the test.js file and the logic.js files are connected
 var logic = require('./logic');
 
 test('Example test', function(t) {
-  t.pass();
+  const actual = 1;
+  const expected = 1;
+  t.pass(actual, expected, "1 should return 1");
   t.end();
 });
+
+// test('Example test', function(t) {
+//   t.pass();
+//   t.end();
+// });
